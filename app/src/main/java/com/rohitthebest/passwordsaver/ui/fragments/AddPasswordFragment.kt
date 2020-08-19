596f072904a5757b0a5c1fb1f466e49ad6b3922f
Mohit Kumar <mohitk02082000@gmail.com>
2020-08-19 20:39:48 +0530

    Working on AddPasswordFragment.kt Added a class for checking password

--- app/src/main/java/com/rohitthebest/passwordsaver/ui/fragments/AddPasswordFragment.kt
+++ app/src/main/java/com/rohitthebest/passwordsaver/ui/fragments/AddPasswordFragment.kt
@@ -1,16 +1,24 @@
 package com.rohitthebest.passwordsaver.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.rohitthebest.passwordsaver.R
 import com.rohitthebest.passwordsaver.databinding.FragmentAddPasswordBinding
+import com.rohitthebest.passwordsaver.other.Constants.EDITTEXT_EMPTY_MESSAGE
+import com.rohitthebest.passwordsaver.other.Functions
+import com.rohitthebest.passwordsaver.util.CheckPasswordPattern
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class AddPasswordFragment : Fragment(R.layout.fragment_add_password) {
+class AddPasswordFragment : Fragment(R.layout.fragment_add_password), View.OnClickListener {
 
-    private var _binding : FragmentAddPasswordBinding? = null
+    private var _binding: FragmentAddPasswordBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -26,6 +34,137 @@ class AddPasswordFragment : Fragment(R.layout.fragment_add_password) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initListeners()
+        textWatcher()
+    }
+
+    private fun initListeners() {
+
+        binding.backBtn.setOnClickListener(this)
+        binding.saveBtn.setOnClickListener(this)
+        binding.checkPasswordStrengthBtn.setOnClickListener(this)
+        binding.mainCL.setOnClickListener(this) // for closing keyboard
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+
+            binding.saveBtn.id -> {
+
+                //todo : save password to database
+            }
+
+            binding.backBtn.id -> {
+
+                requireActivity().onBackPressed()
+            }
+
+            binding.checkPasswordStrengthBtn.id -> {
+
+                if (binding.passwordStrengthCL.visibility == View.VISIBLE) {
+
+                    hideCheckPassStrengthCL()
+                } else {
+                    showCheckPassStrengthCL()
+                }
+            }
+
+        }
+
+        //closing keyboard if opened
+        CoroutineScope(Dispatchers.IO).launch {
+
+            Functions.closeKeyboard(requireActivity())
+        }
+    }
+
+    private fun textWatcher() {
+
+
+        binding.accountNameET.editText?.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(str: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                if (str?.isEmpty()!!) {
+
+                    binding.accountNameET.error = EDITTEXT_EMPTY_MESSAGE
+                } else {
+
+                    binding.accountNameET.error = null
+                }
+            }
+        })
+
+        binding.passwordET.editText?.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(str: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                if (str?.isEmpty()!!) {
+
+                    binding.passwordET.error = EDITTEXT_EMPTY_MESSAGE
+                } else {
+
+                    checkPasswordStrength(str.toString())
+                    binding.passwordET.error = null
+                }
+            }
+        })
+    }
+
+    private fun checkPasswordStrength(passwordString: String) {
+
+        if (CheckPasswordPattern().check_for_a_capital_letter(passwordString) &&
+            CheckPasswordPattern().check_for_a_small_letter(passwordString)
+        ) {
+
+            binding.uppercaseLowercaseIndicator.setImageResource(R.drawable.ic_baseline_check_24)
+        } else {
+
+            binding.uppercaseLowercaseIndicator.setImageResource(R.drawable.ic_baseline_close_24)
+
+        }
+
+        if (CheckPasswordPattern().check_for_min_length(passwordString)) {
+
+            binding.atleast8CharacterIndicator.setImageResource(R.drawable.ic_baseline_check_24)
+
+        } else {
+
+            binding.atleast8CharacterIndicator.setImageResource(R.drawable.ic_baseline_close_24)
+
+        }
+
+        if (CheckPasswordPattern().check_for_special_character(passwordString)) {
+
+            binding.specialCharacterIndicator.setImageResource(R.drawable.ic_baseline_check_24)
+
+        } else {
+
+            binding.specialCharacterIndicator.setImageResource(R.drawable.ic_baseline_close_24)
+
+        }
+
+        if (CheckPasswordPattern().check_for_digit(passwordString)) {
+
+            binding.aDigitIndicator.setImageResource(R.drawable.ic_baseline_check_24)
+        } else {
+
+            binding.aDigitIndicator.setImageResource(R.drawable.ic_baseline_close_24)
+
+        }
+
+        if (passwordString.contains("<") || passwordString.contains(">")) {
+
+            binding.dontuseIndicator.setImageResource(R.drawable.ic_baseline_close_24)
+        } else {
+
+            binding.dontuseIndicator.setImageResource(R.drawable.ic_baseline_check_24)
+        }
 
     }
 
@@ -33,6 +172,26 @@ class AddPasswordFragment : Fragment(R.layout.fragment_add_password) {
         super.onDestroyView()
 
         _binding = null
+
+        //closing keyboard if opened
+        try {
+            CoroutineScope(Dispatchers.IO).launch {
+
+                Functions.closeKeyboard(requireActivity())
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun showCheckPassStrengthCL() {
+
+        binding.passwordStrengthCL.visibility = View.VISIBLE
+    }
+
+    private fun hideCheckPassStrengthCL() {
+
+        binding.passwordStrengthCL.visibility = View.INVISIBLE
     }
 
 }
---
+++ app/src/main/java/com/rohitthebest/passwordsaver/util/CheckPasswordPattern.kt
@@ -0,0 +1,67 @@
+package com.rohitthebest.passwordsaver.util
+
+import java.util.regex.Pattern
+
+class CheckPasswordPattern {
+
+    companion object {
+        private val PASSWORD_PATTERN: Pattern =
+            Pattern.compile("^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#\$%^&+=])(?=\\S+\$).{6,}\$")
+
+        private val a_digit_pattern: Pattern =
+            Pattern.compile(".*?[0-9].*?")
+
+        private val a_letter_pattern: Pattern =
+            Pattern.compile(".*?[a-zA-Z].*?")
+
+        private val a_special_character_pattern: Pattern =
+            Pattern.compile(".*?[@#\$%^&+=].*?")
+
+        private val min_length_pattern: Pattern =
+            Pattern.compile(".{8,}")
+
+        private val a_small_letter_pattern: Pattern =
+            Pattern.compile(".*?[a-z].*?")
+
+        private val a_capital_letter_pattern: Pattern =
+            Pattern.compile(".*?[A-Z].*?")
+
+    }
+
+    fun check_password(password: String): Boolean {
+
+        return PASSWORD_PATTERN.matcher(password).matches()
+    }
+
+    fun check_for_digit(password: String): Boolean {
+
+        return a_digit_pattern.matcher(password).matches()
+
+    }
+
+    fun check_for_letter(password: String): Boolean {
+
+        return a_letter_pattern.matcher(password).matches()
+    }
+
+    fun check_for_special_character(password: String): Boolean {
+
+        return a_special_character_pattern.matcher(password).matches()
+    }
+
+    fun check_for_min_length(password: String): Boolean {
+
+        return min_length_pattern.matcher(password).matches()
+    }
+
+    fun check_for_a_small_letter(password: String): Boolean {
+
+        return a_small_letter_pattern.matcher(password).matches()
+    }
+
+    fun check_for_a_capital_letter(password: String): Boolean {
+
+        return a_capital_letter_pattern.matcher(password).matches()
+    }
+
+}
