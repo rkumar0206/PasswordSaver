47689ebe1cf8c1ed34ef5230931d80b41f0ae05a
Mohit Kumar <mohitk02082000@gmail.com>
2020-08-19 20:00:55 +0530

    Layout for fragment_add_password.xml is completed.

--- app/src/main/java/com/rohitthebest/passwordsaver/ui/fragments/AddPasswordFragment.kt
+++ app/src/main/java/com/rohitthebest/passwordsaver/ui/fragments/AddPasswordFragment.kt
@@ -1,7 +1,38 @@
 package com.rohitthebest.passwordsaver.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.rohitthebest.passwordsaver.R
+import com.rohitthebest.passwordsaver.databinding.FragmentAddPasswordBinding
 
 class AddPasswordFragment : Fragment(R.layout.fragment_add_password) {
+
+    private var _binding : FragmentAddPasswordBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentAddPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+
 }
