8ab5533fcc7e86a61645d2dac8eb434f88fb2765
Mohit Kumar <mohitk02082000@gmail.com>
2020-08-20 12:27:58 +0530

    -> Adapter for SavedPassword Recycler view

--- app/src/main/java/com/rohitthebest/passwordsaver/ui/adapters/SavedPasswordRVAdapter.kt
+++ app/src/main/java/com/rohitthebest/passwordsaver/ui/adapters/SavedPasswordRVAdapter.kt
@@ -8,11 +8,80 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.rohitthebest.passwordsaver.R
 import com.rohitthebest.passwordsaver.database.entity.Password
+import com.rohitthebest.passwordsaver.databinding.SavedPasswordAdapterLayoutBinding
+import com.rohitthebest.passwordsaver.other.Constants.SYNCED
 
 class SavedPasswordRVAdapter : ListAdapter<Password,
         SavedPasswordRVAdapter.SavedPasswordViewHolder>(DiffUtilCallBack()) {
 
-    inner class SavedPasswordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private var mListener: OnClickListener? = null
+
+    inner class SavedPasswordViewHolder(private val itemBinding: SavedPasswordAdapterLayoutBinding) :
+        RecyclerView.ViewHolder(itemBinding.root), View.OnClickListener {
+
+        fun setData(password: Password?) {
+
+            itemBinding.apply {
+
+                accountNameTV.text = password?.accountName
+                passwordTV.text = password?.password
+                if (password?.uid == "") {
+
+                    syncBtn.visibility = View.GONE
+                } else {
+
+                    syncBtn.visibility = View.VISIBLE
+
+                    if (password?.isSynced == SYNCED) {
+
+                        syncBtn.setImageResource(R.drawable.ic_baseline_sync_24)
+                    } else {
+
+                        syncBtn.setImageResource(R.drawable.ic_baseline_sync_disabled_24)
+                    }
+                }
+
+                accountNameTV.setOnClickListener(this@SavedPasswordViewHolder)
+            }
+        }
+
+        override fun onClick(v: View?) {
+            val pos = absoluteAdapterPosition
+            when (v?.id) {
+
+                itemBinding.cardView.id -> {
+
+                    if (pos != RecyclerView.NO_POSITION && mListener != null) {
+
+                        mListener?.onItemClickListener(getItem(pos))
+                    }
+                }
+
+                itemBinding.visibilityBtn.id -> {
+
+                    if (pos != RecyclerView.NO_POSITION && mListener != null) {
+
+                        mListener?.onSeePasswordBtnClickListener(getItem(pos))
+                    }
+                }
+
+                itemBinding.copyBtn.id -> {
+
+                    if (pos != RecyclerView.NO_POSITION && mListener != null) {
+
+                        mListener?.onCopyBtnClickListener(getItem(pos))
+                    }
+                }
+
+                itemBinding.syncBtn.id -> {
+
+                    if (pos != RecyclerView.NO_POSITION && mListener != null) {
+
+                        mListener?.onSyncBtnClickListener(getItem(pos))
+                    }
+                }
+            }
+        }
 
     }
 
@@ -36,15 +105,30 @@ class SavedPasswordRVAdapter : ListAdapter<Password,
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedPasswordViewHolder {
 
-        return SavedPasswordViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.saved_password_adapter_layout, parent, false)
+        val itemBinding = SavedPasswordAdapterLayoutBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
         )
+
+        return SavedPasswordViewHolder(itemBinding)
     }
 
     override fun onBindViewHolder(holder: SavedPasswordViewHolder, position: Int) {
 
-
+        holder.setData(getItem(position))
     }
 
+    interface OnClickListener {
+
+        fun onItemClickListener(password: Password?)
+        fun onSyncBtnClickListener(password: Password?)
+        fun onCopyBtnClickListener(password: Password?)
+        fun onSeePasswordBtnClickListener(password: Password?)
+    }
+
+    fun setOnClickListener(listener: OnClickListener) {
+
+        mListener = listener
+    }
 }
