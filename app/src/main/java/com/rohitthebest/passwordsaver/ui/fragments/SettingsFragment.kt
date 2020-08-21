cc3746687e4fbafb351002fc64a0234df6265598
rohit <rkumar8092378845@gmail.com>
2020-08-21 16:18:10 +0530

    -> SettingsFragment.kt work in progress

--- app/src/main/java/com/rohitthebest/passwordsaver/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/rohitthebest/passwordsaver/ui/fragments/SettingsFragment.kt
@@ -1,7 +1,9 @@
 package com.rohitthebest.passwordsaver.ui.fragments
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,23 +11,43 @@ import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.rohitthebest.passwordsaver.R
 import com.rohitthebest.passwordsaver.database.entity.AppSetting
+import com.rohitthebest.passwordsaver.database.entity.Password
 import com.rohitthebest.passwordsaver.databinding.FragmentSettingsBinding
+import com.rohitthebest.passwordsaver.other.Constants
+import com.rohitthebest.passwordsaver.other.Constants.NOT_SYNCED
 import com.rohitthebest.passwordsaver.other.Constants.OFFLINE
 import com.rohitthebest.passwordsaver.other.Constants.ONLINE
+import com.rohitthebest.passwordsaver.other.Functions
 import com.rohitthebest.passwordsaver.ui.viewModels.AppSettingViewModel
+import com.rohitthebest.passwordsaver.ui.viewModels.PasswordViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
+private const val TAG = "SettingsFragment"
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment(), View.OnClickListener, RadioGroup.OnCheckedChangeListener {
 
     private val appSettingViewModel: AppSettingViewModel by viewModels()
+    private val passwordViewModel: PasswordViewModel by viewModels()
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
 
     private var appSetting: AppSetting? = null
+    private var savedPasswordList: ArrayList<Password>? = null
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,10 +62,15 @@ class SettingsFragment : Fragment(), View.OnClickListener, RadioGroup.OnCheckedC
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        disableSaveBtn()
+
+        savedPasswordList = ArrayList()
+
+        mAuth = Firebase.auth
+
         initListeners()
         getAppSetting()
-
-
+        getPasswordList()
     }
 
     private fun getAppSetting() {
@@ -67,6 +94,31 @@ class SettingsFragment : Fragment(), View.OnClickListener, RadioGroup.OnCheckedC
             e.printStackTrace()
         }
     }
+
+    private fun getPasswordList() {
+
+        try {
+
+            passwordViewModel.getAllPasswordsList().observe(viewLifecycleOwner, Observer { it ->
+
+                if (it.isNotEmpty()) {
+
+                    it.forEach { password ->
+
+                        savedPasswordList?.add(password)
+                    }
+
+                    enableSaveBtn()
+                }
+
+            })
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
 
     private fun updateUI() {
 
@@ -146,6 +198,14 @@ class SettingsFragment : Fragment(), View.OnClickListener, RadioGroup.OnCheckedC
                 } else {
                     //todo : upload appSetting to database
                     //todo : update the field isSynced in sqlite database
+
+                    savedPasswordList?.forEach { password ->
+
+                        password.isSynced = NOT_SYNCED
+                        password.uid = mAuth.currentUser?.uid
+                        passwordViewModel.insert(password)
+                    }
+
                     ONLINE
                 }
 
@@ -162,22 +222,145 @@ class SettingsFragment : Fragment(), View.OnClickListener, RadioGroup.OnCheckedC
                 "${getString(R.string.offline_text)}\n2 .If you choose offline mode all " +
                         "the passwords saved on cloud will" +
                         " be permanently deleted and cannot be retrieved again."
-            showAlertMessage(message)
+
+            AlertDialog.Builder(requireContext())
+                .setMessage(message)
+                .setPositiveButton("Ok") { dialogInterface, _ ->
+                    dialogInterface.dismiss()
+                }.create()
+                .show()
+
         } else {
-            showAlertMessage(getString(R.string.online_text))
+
+            val message = "${getString(R.string.online_text)}\n\nIn Order to save your data to" +
+                    " cloud you need to signIn with your Google Account."
+
+            AlertDialog.Builder(requireContext())
+                .setMessage(message)
+                .setPositiveButton("SignIn") { dialogInterface, _ ->
+
+                    if (Functions.isInternetAvailable(requireContext())) {
+                        disableSaveBtn()
+                        signIn()
+                    } else {
+                        Functions.showToast(requireContext(), Constants.NO_INTERNET_MESSAGE)
+                    }
+                    dialogInterface.dismiss()
+                }.create()
+                .show()
+
         }
 
     }
 
-    private fun showAlertMessage(message: String) {
-
-        AlertDialog.Builder(requireContext())
-            .setMessage(message)
-            .setPositiveButton("Ok") { dialogInterface, _ ->
-                dialogInterface.dismiss()
-            }.create()
-            .show()
+    // [START signin]
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(
+            signInIntent,
+            Constants.RC_SIGN_IN
+        )
     }
+    // [END signin]
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mAuth = Firebase.auth
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == Constants.RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+
+                firebaseAuthWithGoogle(account.idToken!!)
+
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // [START_EXCLUDE]
+                Functions.showToast(requireContext(), "SignIn Un-successful")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+
+        showProgressBar()
+
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+
+                    Functions.showToast(requireContext(), "SignIn successful")
+                    Functions.showToast(requireContext(), "signInWithCredential:success")
+
+                    enableSaveBtn()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Functions.showToast(requireContext(), "Authentication Failed.")
+
+                    binding.modeChangeRG.check(binding.offlineModeRB.id)
+                    enableSaveBtn()
+                    hideProgressBar()
+                }
+                hideProgressBar()
+            }
+    }
+
+    private fun showProgressBar() {
+
+        try {
+            binding.progressBar.visibility = View.VISIBLE
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun hideProgressBar() {
+        try {
+            binding.progressBar.visibility = View.GONE
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun enableSaveBtn() {
+
+        try {
+            binding.saveBtn.isEnabled = true
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun disableSaveBtn() {
+        try {
+            binding.saveBtn.isEnabled = false
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
